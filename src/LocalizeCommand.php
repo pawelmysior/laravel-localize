4d52576fbd3c5017d3c58890451f0762007a2a36
Ugur Aydogdu <ugur.aydogdu@epigra.com>
2020-04-29 00:59:13 +0300

    execute method returns 0 as expected in laravel 7

--- src/LocalizeCommand.php
+++ src/LocalizeCommand.php
@@ -18,7 +18,7 @@ class LocalizeCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if (!$this->isLaravelApplication()) {
+        if (! $this->isLaravelApplication()) {
             $output->writeLn('<error>Not a Laravel application</error>');
 
             return 0;
@@ -26,8 +26,8 @@ class LocalizeCommand extends Command
 
         $languageFilesInstaller = new LanguageFilesInstaller($input->getArgument('lang'));
 
-        if (!$languageFilesInstaller->languageExists()) {
-            $output->writeLn('<error>Couldn\'t find "' . $input->getArgument('lang') . '" language files.</error>');
+        if (! $languageFilesInstaller->languageExists()) {
+            $output->writeLn('<error>Couldn\'t find "'.$input->getArgument('lang').'" language files.</error>');
 
             return 0;
         }
@@ -44,13 +44,15 @@ class LocalizeCommand extends Command
         $languageFilesInstaller->downloadJsonLanguageFile();
         $output->writeln('<info> success</info>');
 
-        $output->writeLn('<info>Successfully installed the "' . $input->getArgument('lang') . '" language files!</info>');
+        $output->writeLn('<info>Successfully installed the "'.$input->getArgument('lang').'" language files!</info>');
         $output->writeln('<info>Don\'t forget to change the value of the locale key in "config/app.php".</info>');
         $output->writeln('<info>Happy coding!</info>');
+
+        return 0;
     }
 
     protected function isLaravelApplication()
     {
-        return file_exists(getcwd() . '/artisan');
+        return file_exists(getcwd().'/artisan');
     }
 }
