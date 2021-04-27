3d559b45773d2ef0418b6c6a9742558ed5dd22e9
Ugur Aydogdu <ugur.aydogdu@epigra.com>
2021-04-27 22:08:47 +0300

    changed to new repo address
    caouecs/laravel-lang is deprecated. now all files are moved to https://github.com/Laravel-Lang/lang

--- src/LanguageFilesInstaller.php
+++ src/LanguageFilesInstaller.php
@@ -33,17 +33,18 @@ class LanguageFilesInstaller
     public function downloadPhpLanguageFiles()
     {
         foreach ($this->files as $file) {
-            $this->downloadLanguageFile('/src/' . $this->lang . '/' . $file, $this->lang . '/' . $file);
+            $this->downloadLanguageFile($this->lang . '/' . $file, $this->lang . '/' . $file);
         }
     }
 
     public function downloadJsonLanguageFile()
     {
-        $this->downloadLanguageFile('/json/' . $this->lang . '.json', $this->lang . '.json');
+        $this->downloadLanguageFile($this->lang.'/'.$this->lang . '.json', $this->lang . '.json');
     }
 
     protected function downloadLanguageFile($sourceFile, $destinationFile)
     {
+        var_dump($sourceFile);
         if ($this->fileExists($sourceFile)) {
             $contents = $this->getLanguageFileContents($sourceFile);
 
@@ -55,8 +56,7 @@ class LanguageFilesInstaller
     {
         $httpStatus = get_headers($this->getGithubRepositoryPath() . '/' . $file)[0];
 
-        return (bool)strpos($httpStatus, '200');
-        return (bool)strpos($httpStatus, '200') || (bool) strpos($httpStatus, '301');
+        return (bool)strpos($httpStatus, '200') || (bool)strpos($httpStatus, '301');
     }
 
     protected function getLanguageFileContents($file)
@@ -71,7 +71,7 @@ class LanguageFilesInstaller
 
     protected function getGithubRepositoryPath()
     {
-        return 'https://raw.githubusercontent.com/caouecs/Laravel-lang/master';
+        return 'https://raw.githubusercontent.com/Laravel-Lang/lang/master/locales';
     }
 
     protected function getSpecificLanguageDirectoryPath()
